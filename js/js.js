99b102d624d20d5ff133fa8449702a35653db72d
Narek Martirosyan <narekmartirosyanbm21@gmail.com>
2024-04-22 16:06:24 +0400

    1

--- js/js.js
+++ js/js.js
@@ -1,43 +1,44 @@
-const carousel = document.querySelector(".section5slider");
-const carouselBoxes = document.querySelectorAll(".section5sliderBox");
+$(document).ready(function() {
+    const $carousel = $(".section5slider");
+    const $carouselBoxes = $(".section5sliderBox");
 
-let isDragging = false;
-let startX;
-let scrollLeft;
+    let isDragging = false;
+    let startX;
+    let scrollLeft;
 
-carousel.addEventListener("mousedown", (e) => {
-    isDragging = true;
-    startX = e.pageX - carousel.offsetLeft;
-    scrollLeft = carousel.scrollLeft;
+    $carousel.on("mousedown", function(e) {
+        isDragging = true;
+        startX = e.pageX - $carousel.offset().left;
+        scrollLeft = $carousel.scrollLeft();
+    });
+
+    $carousel.on("mouseleave", function() {
+        isDragging = false;
+    });
+
+    $carousel.on("mouseup", function() {
+        isDragging = false;
+    });
+
+    $carousel.on("mousemove", function(e) {
+        if (!isDragging) return;
+        e.preventDefault();
+        const x = e.pageX - $carousel.offset().left;
+        const walk = x - startX;
+        $carousel.scrollLeft(scrollLeft - walk);
+    });
+
+    const $prevButton = $(".prev-button");
+    const $nextButton = $(".next-button");
+
+    $prevButton.on("click", function() {
+        $carousel.scrollLeft($carousel.scrollLeft() - $carousel.outerWidth());
+    });
+
+    $nextButton.on("click", function() {
+        $carousel.scrollLeft($carousel.scrollLeft() + $carousel.outerWidth());
+    });
+    $(".menu").click(function (){
+        alert("Message")
+    })
 });
-
-carousel.addEventListener("mouseleave", () => {
-    isDragging = false;
-});
-
-carousel.addEventListener("mouseup", () => {
-    isDragging = false;
-});
-
-carousel.addEventListener("mousemove", (e) => {
-    if (!isDragging) return;
-    e.preventDefault();
-    const x = e.pageX - carousel.offsetLeft;
-    const walk = x - startX;
-    carousel.scrollLeft = scrollLeft - walk;
-});
-
-const prevButton = document.querySelector(".prev-button");
-const nextButton = document.querySelector(".next-button");
-
-prevButton.addEventListener("click", () => {
-    carousel.scrollLeft -= carousel.offsetWidth;
-});
-
-nextButton.addEventListener("click", () => {
-    carousel.scrollLeft += carousel.offsetWidth;
-});
-$('#menu').click(function (){
-    alert("Menu button")
-})
-
